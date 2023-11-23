92e7dccb2784f4b376746e9a5f36ba285d7c8285
abmantis <amfcalt@gmail.com>
2023-11-23 22:30:25 +0000

    Add Korvo1 board

---
+++ component.mk
@@ -0,0 +1,9 @@
+#
+# "main" pseudo-component makefile.
+#
+# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+
+ifdef CONFIG_AUDIO_BOARD_CUSTOM
+COMPONENT_ADD_INCLUDEDIRS += ./esp32_s3_korvo1
+COMPONENT_SRCDIRS += ./esp32_s3_korvo1
+endif
